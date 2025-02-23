1e403b12dd6fb86fa109cc33db6795d63934474c
Kasperissim0 <dankor42q@gmail.com>
2025-02-23 13:17:41 +0100

    Update test.cpp

--- Code/test.cpp
+++ Code/test.cpp
@@ -6,12 +6,19 @@
 #include <limits> // For clearing the input buffer
 #include <chrono> // For time manipulation + delaying actions
 #include <thread> // For time manipulation + delaying actions
+#include <cmath> // For mathematical calculations
+#include <random> // For generating random numbers
+
 
 using namespace std;
 
 int main () {
 
-  // here
+ system("clear");
+
+
+ // here
+ 
 
   return 0;  
 }
