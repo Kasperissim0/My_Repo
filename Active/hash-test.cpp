75c4d16312b6bd6cc19a6e4062fdd39cdf9b1156
Kasperissim0 <dankor42q@gmail.com>
2025-05-29 13:33:14 +0200

    Changed Folder Titles
    	modified:   .vscode/tasks.json
    	new file:   Active/hash-test.cpp
    	renamed:    Code/rebuild.cpp -> Active/rebuild.cpp
    	renamed:    Code/test.cpp -> Active/test.cpp
    	deleted:    Archive/Major/.DS_Store
    	renamed:    Archive/Major/Message_Board_3.0.cpp -> Archive/Single-File/Major/Message_Board_3.0.cpp
    	renamed:    Archive/Major/Xvs0.cpp -> Archive/Single-File/Major/Xvs0.cpp
    	renamed:    Archive/Major/blackjack_terminal.cpp -> Archive/Single-File/Major/blackjack_terminal.cpp
    	renamed:    Archive/Major/interactable_matrix.cpp -> Archive/Single-File/Major/interactable_matrix.cpp
    	renamed:    Archive/Medium/2d_arrays.cpp -> Archive/Single-File/Medium/2d_arrays.cpp
    	renamed:    Archive/Medium/3+d_arrays.cpp -> Archive/Single-File/Medium/3+d_arrays.cpp
    	renamed:    Archive/Medium/Message_Board_1.0.cpp -> Archive/Single-File/Medium/Message_Board_1.0.cpp
    	renamed:    Archive/Medium/Message_Board_2.0.cpp -> Archive/Single-File/Medium/Message_Board_2.0.cpp
    	renamed:    Archive/Medium/blockchain.cpp -> Archive/Single-File/Medium/blockchain.cpp
    	renamed:    Archive/Medium/int_closest_to_number.cpp -> Archive/Single-File/Medium/int_closest_to_number.cpp
    	renamed:    Archive/Medium/name_flip.cpp -> Archive/Single-File/Medium/name_flip.cpp
    	renamed:    Archive/Medium/prime.cpp -> Archive/Single-File/Medium/prime.cpp
    	renamed:    Archive/Minor/circ_area.cpp -> Archive/Single-File/Minor/circ_area.cpp
    	renamed:    Archive/Minor/coin_flip.cpp -> Archive/Single-File/Minor/coin_flip.cpp
    	renamed:    Archive/Minor/conversion.cpp -> Archive/Single-File/Minor/conversion.cpp
    	renamed:    Archive/Minor/dyn_memory.cpp -> Archive/Single-File/Minor/dyn_memory.cpp
    	renamed:    Archive/Minor/interactive_tax.cpp -> Archive/Single-File/Minor/interactive_tax.cpp
    	renamed:    Archive/Minor/interplanetary_warrior.cpp -> Archive/Single-File/Minor/interplanetary_warrior.cpp
    	renamed:    Archive/Minor/order_sum.cpp -> Archive/Single-File/Minor/order_sum.cpp
    	renamed:    Archive/Minor/pointers.cpp -> Archive/Single-File/Minor/pointers.cpp
    	renamed:    Archive/Minor/prime_recursive.cpp -> Archive/Single-File/Minor/prime_recursive.cpp
    	renamed:    Archive/Minor/random_number.cpp -> Archive/Single-File/Minor/random_number.cpp
    	renamed:    Archive/Minor/reverse sentence test 1.cpp -> Archive/Single-File/Minor/reverse sentence test 1.cpp
    	renamed:    Archive/Minor/reverse sentence test 2.cpp -> Archive/Single-File/Minor/reverse sentence test 2.cpp
    	renamed:    Archive/Minor/tax_calculator.cpp -> Archive/Single-File/Minor/tax_calculator.cpp
    	renamed:    Archive/Minor/trim_space.cpp -> Archive/Single-File/Minor/trim_space.cpp
    	new file:   Build/hash-test
    	renamed:    Build/test.dSYM/Contents/Info.plist -> Build/hash-test.dSYM/Contents/Info.plist
    	new file:   Build/hash-test.dSYM/Contents/Resources/DWARF/hash-test
    	renamed:    Build/hello.dSYM/Contents/Resources/Relocations/aarch64/hello.yml -> Build/hash-test.dSYM/Contents/Resources/Relocations/aarch64/hash-test.yml
    	deleted:    Build/hello
    	deleted:    Build/hello.dSYM/Contents/Info.plist
    	deleted:    Build/hello.dSYM/Contents/Resources/DWARF/hello
    	deleted:    Build/test
    	deleted:    Build/test.dSYM/Contents/Resources/DWARF/test
    	deleted:    Build/test.dSYM/Contents/Resources/Relocations/aarch64/test.yml
    	deleted:    Code/.DS_Store

---
+++ Active/hash-test.cpp
@@ -0,0 +1,66 @@
+#include <iostream> // For output and input
+#include <iomanip> // For formatting output nicely
+#include <string> // For strings
+#include <vector> // For vectors
+#include <cstdlib> // For clearing the terminal
+#include <limits> // For clearing the input buffer
+#include <chrono> // For time manipulation + delaying actions
+#include <thread> // For time manipulation + delaying actions
+#include <cmath> // For mathematical calculations
+#include <random> // For generating random numbers
+#include <SFML/Graphics.hpp> // For Graphics
+
+using namespace std;
+
+int GetRandomNumber(const int MinNumber = 1, const int MaxNumer = 999999999, const bool ReturnPrime = false) {
+
+  random_device RandomSeed;
+  mt19937 GenerateNumber(RandomSeed());
+  uniform_int_distribution<> DistributionRange(MinNumber, MaxNumer);
+  int OriginalNumber = DistributionRange(GenerateNumber);
+
+  if (ReturnPrime) { // If Prime
+
+    bool NumberIsPrime = true;
+
+    if (OriginalNumber < 2) NumberIsPrime = false; // Not Prime
+
+    for (int i = 2; i <= sqrt(OriginalNumber); i++) { // Check All Possibilities
+
+      if (OriginalNumber % i == 0) { // Not Prime
+          NumberIsPrime = false; 
+          break;
+      }
+
+    }
+
+    return NumberIsPrime ? OriginalNumber : GetRandomNumber(MinNumber, MaxNumer, true); // Prime OR Run Again
+  }
+
+  else {
+
+    return OriginalNumber; // Normal Number
+
+  }
+}
+
+void DisplayData() {
+
+
+
+}
+
+
+
+int main () {
+  
+  system("clear");
+
+  int ArraySize = GetRandomNumber(1, 100);
+  int *size = &ArraySize;
+  cout << *size << endl;
+  int array[*size];
+
+
+  return 0;
+}
