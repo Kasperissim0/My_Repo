ebe78e43c955331c0e7297d94d631d17329c34a5
Kasperissim0 <dankor42q@gmail.com>
2025-11-04 18:47:27 +0100

    fixed PR1 classwork

--- University/2025w/5th/list.cpp
+++ University/2025w/5th/list.cpp
@@ -2,17 +2,16 @@
 #include<vector>
 #include<string>
 #include<cmath>
-#include "/home/korniienkd07/2025w/functions/screen.h"
+#include<iomanip> // remove if using clearScreen();
+// #include "/home/korniienkd07/University/2025w/functions/screen.h"
 
 using namespace std;
 
-static const size_t LIST_LENGTH = 4; // Set List Clumping Here
-// TODO FIX currently cannot set to 0 since modulo is used later
+static const size_t LIST_CAPACITY = 3; //! Set List Clumping Here, 0 for 1 single list
 
 vector<string> splitVectors(vector<vector<string>>& elements, int returnVector) {
 	return elements.at(returnVector);
 }
-
 string turnIntoList(vector<string>& elements, int changeLength = 0) {
 	string list = ""; 
 	size_t listLength = (changeLength ? changeLength : elements.size());
@@ -20,76 +19,66 @@ string turnIntoList(vector<string>& elements, int changeLength = 0) {
 	
 	for(size_t i = 0; i < listLength; i++) {
 		if (i < (listLength - 2)) list += (elements.at(i) + ", ");
-		else if (i < (listLength - 1)) list += (elements.at(i) + " und ");
+		else if (i < (listLength - 1)) list += (elements.at(i) + " and ");
 		else list += (elements.at(i) + "."); 
 	}
 	return list;
 }
 
 int main() {
-	unsigned int inputAmount = 1;
 	vector<string> elements;
-	string tempStorage, spacing = ".    ";
+	string tempStorage, spacing = ".     ";
+
+	static_assert(LIST_CAPACITY != 1, "A List With One Element Makes No Sense");
 	
-	clearScreen();
+	// system("clear"); // clearScreen();
 	cout << "Insert Element #1: ";
-	while (cin >> tempStorage && (tempStorage != "q" || (inputAmount %  LIST_LENGTH) >= 2)) {
-		// DEBUG
-		clog << "Input Amount: " << inputAmount << " | " 
-			  << inputAmount << " % " << LIST_LENGTH << " = " 
-			  << (inputAmount %  LIST_LENGTH) << endl;
-		// DEBUG
-		// TODO FIX currently increments inputAmount ONCE after input "q"
-		if (tempStorage != "q") { elements.push_back(tempStorage); inputAmount++; }
+	while (cin >> tempStorage && (tempStorage != "q" || elements.size() < 2 || (LIST_CAPACITY != 0 && elements.size() % LIST_CAPACITY == 1))) {
+		if (tempStorage != "q") { elements.push_back(tempStorage); }
+		else if (elements.size() > 2 && (elements.size() % LIST_CAPACITY != 1)) break;
 		cout << "Insert Element #" << (elements.size() + 1) << ": ";
 		tempStorage =  "";
+		// if (LIST_CAPACITY != 0) {
+		// 	clog << "Input Amount: " << (elements.size() + 1) << " | " 
+		// 	     << (elements.size() + 1) << " % " << LIST_CAPACITY << " = " 
+		// 	     << ((elements.size() + 1) %  LIST_CAPACITY) << endl;
+		// }
 	} 
-	clearScreen();
-	if (LIST_LENGTH) {
-		size_t listAmount = round(inputAmount / LIST_LENGTH), index = 0;
-		vector<vector<string>> allVectors {{""}};
+	
+	system("clear"); // clearScreen();
+	if (LIST_CAPACITY > 0 && !elements.empty()) {
+		size_t listAmount = ceil((double)elements.size() / LIST_CAPACITY), index = 0;
+		vector<vector<string>> allVectors;
 		allVectors.resize(listAmount);
-		// DEBUG
-		clog << "List Amount: " << listAmount << endl;
-		// DEBUG
+		// clog << "List Amount: " << listAmount << endl;
 		for (size_t a = 0; a < listAmount; a++) {
-			allVectors.at(a).resize(LIST_LENGTH);
-			for (size_t b = 0; b < LIST_LENGTH; b++) {
-				// DEBUG
-				clog << "Assigning: v[" << a << "][" << b << "] = v[" << index 
-					  << "] = " << elements.at(index);
-				// DEBUG
-				allVectors.at(a).at(b) = elements.at(index);
-				index++;
+			allVectors.at(a).resize(LIST_CAPACITY);
+			for (size_t b = 0; b < LIST_CAPACITY; b++) {
 				if (index >= elements.size()) {
-					allVectors.at(a).resize(LIST_LENGTH -1);
-					// DEBUG
-					clog << " | Final v[] size: " << elements.size() << endl;
-					// DEBUG
+					allVectors.at(a).resize(b);
+					// clog << " | Final v[] size: " << b << endl;
 					break; 
 				}
-				// DEBUG
-				clog << " | v[] size: " << elements.size() << endl;
-				// DEBUG
+				// clog << "Assigning: v[" << a << "][" << b << "] = v[" << index 
+				// 	   << "] = " << elements.at(index);
+				allVectors.at(a).at(b) = elements.at(index);
+				index++;
+				// clog << " | v[] size: " << elements.size() << endl;
 			}
 		}
 		
 		cout << "The List(s) Are: " << endl << endl;
-		for (size_t i = 1; i <= listAmount; i++) {
-			vector<string> current = splitVectors(allVectors, (i - 1));
-		
+		for (size_t i = 0; i < listAmount; i++) {
+			vector<string> current = splitVectors(allVectors, i);
+
 			// Dynamic Space Scaling
-			if (i == 10) spacing.pop_back();
-			if (i == 100) spacing.pop_back();
-			if (i == 1000) spacing.pop_back();
+			if ((i + 1) == 10) spacing.pop_back();
+			if ((i + 1) == 100) spacing.pop_back();
+			if ((i + 1) == 1000) spacing.pop_back();
 			
-			cout << i << spacing << turnIntoList(current, LIST_LENGTH) 
-				  << endl;
+			cout << (i + 1) << spacing << turnIntoList(current, LIST_CAPACITY) << endl;
 		}
 	}
-	else {
-		cout << "The List Is: " << endl << endl << turnIntoList(elements) << endl;
-	}
-	// cout <<  endl << inputAmount << endl; // Troubleshooting
+	else cout << "The List Is: " << endl << endl << turnIntoList(elements) << endl;
 	return 0;
 }
